eaaba5a0b9433c806ba9d2062bd53b00a2c51e32
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-08-15 23:22:47 -0500

    Add text sizing buttons.

--- script.js
+++ script.js
@@ -1,38 +1,64 @@
-const changeTextSizeBtn = document.querySelector("#btn-change-text-size");
+const changeTextSizeBtns = document.querySelectorAll(".btn-change-text-size");
 const showImagesBtn = document.querySelector("#btn-show-hide-images");
 const showFavsBtn = document.querySelector("#btn-show-favs");
+
+let defaultSizeStr = "small";
+let [defaultSize] = Array.from(changeTextSizeBtns).filter(
+  (btn) => btn.getAttribute("data-size") === defaultSizeStr
+);
+
 let startingFontSize = parseInt(
   window
     .getComputedStyle(document.documentElement)
     .getPropertyValue("--font-size-default")
 );
-let currentFontSize = parseInt(startingFontSize);
 
 const savedArticlesDiv = document.querySelector("#saved-article-container");
 
 // EVENT LISTENERS
-changeTextSizeBtn.addEventListener("click", () => handleTextSizeChange());
+changeTextSizeBtns.forEach((btn) =>
+  btn.addEventListener("click", (e) =>
+    handleTextSizeChange(e.target.parentElement)
+  )
+);
 
 showImagesBtn.addEventListener("click", () => handleShowImages());
-
 showFavsBtn.addEventListener("click", () => handleShowFavs());
 
 // HANDLER FUNCTIONS
-function handleTextSizeChange() {
-  let proposedSize = (currentFontSize += 4);
+function handleTextSizeChange(button) {
+  let size = button.getAttribute("data-size");
+  toggleButtonState(button);
+  let newSize;
 
-  if (currentFontSize < startingFontSize * 2) {
-    currentFontSize = proposedSize;
-  } else {
-    currentFontSize = startingFontSize;
+  switch (size) {
+    case "small":
+      newSize = 16;
+      break;
+    case "medium":
+      newSize = 24;
+      break;
+    case "large":
+      newSize = 32;
+      break;
   }
 
   document.documentElement.style.setProperty(
     "--font-size-default",
-    `${proposedSize}px`
+    `${newSize}px`
   );
 
-  console.log("text is now", currentFontSize, "px");
+  //   console.log("text is now", newSize, "px");
+}
+
+function toggleButtonState(activeBtn) {
+  changeTextSizeBtns.forEach((btn) => {
+    if (activeBtn.getAttribute("data-size") === btn.getAttribute("data-size")) {
+      btn.classList.add("active");
+    } else {
+      btn.classList.remove("active");
+    }
+  });
 }
 
 function handleArticleFetch(url) {
@@ -54,3 +80,7 @@ function checkArticleFavoriteStatus(buttonStatus) {
 
   return buttonStatus === "added" ? removeIcon : addIcon;
 }
+
+window.addEventListener("DOMContentLoaded", () =>
+  handleTextSizeChange(defaultSize)
+);
